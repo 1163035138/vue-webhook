0e592832483cc54d523781464c89ef92bcb6abf5
zhangyadong <1163035138@qq.com>
2025-05-18 17:05:51 +0800

    4

--- vue-back.sh
+++ vue-back.sh
@@ -2,7 +2,7 @@
 WORK_PATH='/usr/projects/my-nest-project'
 cd $WORK_PATH
 echo "先清除老代码"
-git reset --hard origin/master
+git reset --hard origin/main
 git clean -f
 echo "拉取最新代码"
 git pull origin master
