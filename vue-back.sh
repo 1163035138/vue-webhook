ecefc1f621d2da2d2624b44f90972fc1d45a1771
zhangyadong <1163035138@qq.com>
2025-05-18 16:55:33 +0800

    5

---
+++ vue-back.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+WORK_PATH='/usr/projects/my-nest-project'
+cd $WORK_PATH
+echo "先清除老代码"
+git reset --hard origin/master
+git clean -f
+echo "拉取最新代码"
+git pull origin master
+echo "开始构建"
+docker build -t my-nest-project .
+echo "停止旧容器并删除旧容器"
+docker stop my-nest-project-container
+docker rm my-nest-project-container
+echo "启动新容器"
+docker container run -p 3000:3000 --name my-nest-project-container -d my-nest-project
