cbd5558d26b12afc36b8bba304fe7c767b3fd9d8
zhangyadong <1163035138@qq.com>
2025-05-18 17:58:20 +0800

    2

--- vue-back.sh
+++ vue-back.sh
@@ -7,7 +7,7 @@ git clean -f
 echo "拉取最新代码"
 git pull
 echo "开始构建"
-docker build -t my-nest-project .
+docker build -t my-nest-project:1.0.0 .
 echo "停止旧容器并删除旧容器"
 docker stop my-nest-project-container
 docker rm my-nest-project-container
